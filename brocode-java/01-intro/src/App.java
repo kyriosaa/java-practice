f13016ce4d52a592b6b274b4ec43d93bf81c1d25
kyriosaa <kyriosaaph@gmail.com>
2025-03-12 13:56:38 +0700

    First commit

---
+++ brocode-java/01-intro/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // This is my first Java program!
+
+        System.out.print("I like tacos!\n");        // use \n
+        System.out.println("It's really tasty");    // or println()
+        System.out.println("Buy me tacos!");
+    }
+}
