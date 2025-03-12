414dc5d015723d0e6fc4c66c867c56036875319c
kyriosaa <kyriosaaph@gmail.com>
2025-03-12 15:47:23 +0700

    05-arithmetic

---
+++ brocode-java/05-arithmetic/src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Arithmetic Operators
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        // z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x / y;
+        // z = x % y;
+
+        // Augmented Assignment Operators
+        // x += y;
+        // x -= y;
+        // x *= y;
+        // x /= y;
+
+        // Increment and Decrement Operators
+        // x++;
+        // x--;
+
+        // Order of Operations follows PEMDAS
+
+        System.out.println(z);
+    }
+}
