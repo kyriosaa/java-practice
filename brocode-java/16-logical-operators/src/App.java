c1f7325ed594864e295f85ef5fc2ad72f86df8e4
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 14:49:39 +0700

    16-logical-operators

--- brocode-java/16-logical-operators/src/App.java
+++ brocode-java/16-logical-operators/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // && = AND
+        // || = OR
+        // !  = NOT
+
+        double temp = 20;
+
+        if(temp <= 30 && temp >= 0) {
+            System.out.println("The weather is nice!");
+        } else {
+            System.out.println("The weather is terrible!");
+        }
     }
 }
