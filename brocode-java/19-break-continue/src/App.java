ac5a54b76b6116da4d5130394c4f8a6c44faccdb
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 15:17:15 +0700

    19-break-continue

--- brocode-java/19-break-continue/src/App.java
+++ brocode-java/19-break-continue/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        
+        // break    = break out of a loop (STOP)
+        // continue = skip current iteration of a loop (SKIP)
+
+        for(int i = 1; i <= 10; i++) {
+            if(i == 5) {
+                continue;
+            } else if(i == 9) {
+                break;
+            }
+            System.out.println(i);
+            Thread.sleep(100);
+        }
     }
 }
