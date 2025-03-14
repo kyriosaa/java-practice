3c3b5c50530aac1a2d0887376bb01edcc796c18b
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 13:46:36 +0700

    13-string-methods

--- brocode-java/13-string-methods/src/App.java
+++ brocode-java/13-string-methods/src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String name = "   Kin   ";
+
+        int length = name.length();
+        char letter = name.charAt(4);
+        int index = name.indexOf("i");
+        int lastIndex = name.lastIndexOf("n");
+
+        // name = name.toUpperCase();
+        // name = name.toLowerCase();
+        // name = name.trim();
+        name = name.replace("K", "G");
+        System.out.println(name);
+
+        System.out.println(length);
+        System.out.println(letter);
+        System.out.println(index);
+        System.out.println(lastIndex);
     }
 }
