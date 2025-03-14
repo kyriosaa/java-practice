f14acef30f636be9f15de9e3dd705382c7bbda5d
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 20:41:11 +0700

    28-oop

--- brocode-java/28-oop/src/App.java
+++ brocode-java/28-oop/src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // Object = An entity that holds data (attributes) and can perform actions (methods).
+        //          It is a reference data type.
+
+        Car car = new Car();
+
+        System.out.println(car.make);
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println(car.price);
+        System.out.println(car.isRunning);
+        System.out.println();
+
+        if(car.isRunning) {
+            System.out.printf("%s is running", car.model);
+        } else {
+            System.out.printf("%s is not running", car.model);
+        }
     }
 }
---
+++ brocode-java/28-oop/src/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+
+    String make = "Ford";
+    String model = "Mustang";
+    int year = 2025;
+    double price = 58000.99;
+    boolean isRunning = false;
+    
+}
