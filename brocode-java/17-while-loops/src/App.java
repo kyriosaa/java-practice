977bec16883008efe7a39f4c4d14eb6191d5b59c
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 15:09:29 +0700

    17-while-loops

--- brocode-java/17-while-loops/src/App.java
+++ brocode-java/17-while-loops/src/App.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // while loop = repeat some code forever while some condition remains true
+
+        Scanner scanner = new Scanner(System.in);
+
+        int age;
+        
+        // --- while loop ---
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
+        
+        while(age < 0) {
+            System.out.println("Your age can't be negative!");
+            System.out.print("Enter your age: ");
+            age = scanner.nextInt();
+        }
+        // ------------------
+        
+        // --- do while loop ---
+        do { 
+            if(age < 0) {
+                System.out.println("Your age can't be negative!");
+            }
+            System.out.print("Please enter your age: ");
+            age = scanner.nextInt();
+        } while (age < 0);
+        // ---------------------
+
+        System.out.printf("You are %d years old", age);
+
+        scanner.close();
     }
 }
