8c599b54274ca27b41a17a8f074abcbfc76a7fe3
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 13:39:43 +0700

    12-nested-if

--- brocode-java/12-nested-if/src/App.java
+++ brocode-java/12-nested-if/src/App.java
@@ -1,5 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean isStudent = true;
+        boolean isSenior = true;
+        double price = 9.99;
+
+        System.out.printf("The ticket price is: $%.2f\n", price);
+
+        if(isStudent) {
+            if(isSenior) {
+                System.out.println("You get a student discount of 10%");
+                System.out.println("You get a senior discount of 20%");
+                price *= 0.7;
+                System.out.printf("Updated ticket price: $%.2f\n", price);
+            } else {
+                System.out.println("You get a student discount of 10%");
+                price *= 0.9;
+                System.out.printf("Updated ticket price: $%.2f\n", price);
+            }
+        } else {
+            if(isSenior) {
+                System.out.println("You get a senior discount of 20%");
+                price *= 0.8;
+                System.out.printf("Updated ticket price: $%.2f\n", price);
+            } else {
+                price *= 1.0;
+            }
+        }
+
     }
 }
