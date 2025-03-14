63103fe1ce7692f8f6cd5c82ef1291ca2cb19b43
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 17:21:25 +0700

    23-variable-scope

--- brocode-java/23-variable-scope/src/App.java
+++ brocode-java/23-variable-scope/src/App.java
@@ -1,5 +1,22 @@
 public class App {
+
+    static int y = 3;  // GLOBAL
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // variable scope = where a variable can be accessed
+
+        int x = 1;  // LOCAL
+        
+        System.out.println("main(x) = " + x);
+        System.out.println("main(y) = " + y);
+        doSomething();
+    }
+    
+    static void doSomething() {
+        int x = 2;  // LOCAL
+
+        System.out.println("doSomething(x) = " + x);
+        System.out.println("doSomething(y) = " + y);
     }
 }
