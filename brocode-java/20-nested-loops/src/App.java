2cd9f177a8099bf22e4c3ca632907413ced96644
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 15:54:21 +0700

    20-nested-loops

--- brocode-java/20-nested-loops/src/App.java
+++ brocode-java/20-nested-loops/src/App.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // nested loop = A loop inside another loop
+        //               Used often in matrices or DS&A
+
+        // for(int i = 1; i <= 3; i++) {
+        //     for(int j = 1; j <= 9; j++) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int columns;
+        char symbol;
+
+        System.out.print("How many rows?: ");
+        rows = scanner.nextInt();
+
+        System.out.print("How many columns?: ");
+        columns = scanner.nextInt();
+
+        System.out.print("Enter a symbol: ");
+        symbol = scanner.next().charAt(0);
+
+        for(int i = 0; i < columns; i++) {
+            for(int j = 0; j < rows; j++) {
+                System.out.print(symbol);
+            }
+            System.out.println();
+        }
+
+        scanner.close();
     }
 }
