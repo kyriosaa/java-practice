1fbfe33f6f2d555a1573552a7a44b16bfe27cbcc
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 17:14:11 +0700

    22-overloaded-methods

--- brocode-java/22-overloaded-methods/src/App.java
+++ brocode-java/22-overloaded-methods/src/App.java
@@ -1,5 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // overloaded methods = methods that share the same name,
+        //                      but different parameters
+        //                      signature = name + parameters
+
+        String bread = "flatbread";
+        String cheese = "cheddar";
+        String topping = "pepperoni";
+
+        String pizza = bakePizza(bread, cheese, topping);
+        System.out.println(pizza);
+    }
+
+    static String bakePizza(String bread) {
+        return bread + " pizza";
+    }
+
+    static String bakePizza(String bread, String cheese) {
+        return bread + " " + cheese + " pizza";
+    }
+
+    static String bakePizza(String bread, String cheese, String topping) {
+        return bread + " " + cheese + " pizza with " + topping;
     }
 }
