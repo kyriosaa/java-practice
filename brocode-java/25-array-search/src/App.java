ff9e3abbb527d4e1381302df86aab393a98da787
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 18:22:21 +0700

    25-array-search

--- brocode-java/25-array-search/src/App.java
+++ brocode-java/25-array-search/src/App.java
@@ -1,5 +1,43 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int[] numbers = {1, 9, 2, 8, 3, 5, 4};
+        int intTarget = 7;
+
+        String[] fruits = {"apple", "orange", "banana"};
+        String stringTarget = "orange";
+
+        boolean isFound = false;
+
+        intSearch(numbers, intTarget, isFound);
+        stringSearch(fruits, stringTarget, isFound);
+    }
+
+    static void intSearch(int[] numbers, int intTarget, boolean isFound) {
+        for(int i = 0; i < numbers.length; i++) {
+            if(intTarget == numbers[i]) {
+                System.out.println("Element found at index: " + i);
+                isFound = true;
+                break;
+            }
+        }
+
+        if(!isFound) {
+            System.out.println("Element not found in array");
+        }
+    }
+
+    static void stringSearch(String[] fruits, String stringTarget, boolean isFound) {
+        for(int i = 0; i < fruits.length; i++) {
+            if(fruits[i].equals(stringTarget)) {
+                System.out.println("Element found at index: " + i);
+                isFound = true;
+                break;
+            }
+        }
+
+        if(!isFound) {
+            System.out.println("Element not found in array");
+        }
     }
 }
