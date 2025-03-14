c655a567a12d0970966cbb83a8fdd73e57115303
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 13:10:50 +0700

    10-printf

--- brocode-java/10-printf/src/App.java
+++ brocode-java/10-printf/src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // printf() = method used to format output
+        // %p[flags][width][.precision][specifier-character]
+
+        String name = "Spongebob";
+        char firstLetter = 'S';
+        int age = 30;
+        double height = 60.5;
+        boolean isEmployed = true;
+
+        System.out.printf("Hello %s\n", name);
+        System.out.printf("Your name starts with an %c\n", firstLetter);
+        System.out.printf("You are %d years old\n", age);
+        System.out.printf("You are %.1f inches tall\n", height);
+        System.out.printf("Employed: %b\n", isEmployed);
+
+        System.out.printf("%s is %d years old and %.1f inches tall.", name, age, height);
     }
 }
