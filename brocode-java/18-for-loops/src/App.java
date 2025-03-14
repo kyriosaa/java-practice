d0eb8dc009993da916c96f6f8dd3e88621586480
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 15:14:21 +0700

    18-for-loops

--- brocode-java/18-for-loops/src/App.java
+++ brocode-java/18-for-loops/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // for loop = execute some code a CERTAIN amount of times
+
+        int countdownStart = 10;
+
+        for(int i = countdownStart; i > 0; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+        
+        System.out.println("HAPPY NEW YEAR");
     }
 }
