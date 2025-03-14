dafb43b6b7446e035449884460a99d75d36faf24
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 18:12:33 +0700

    24-arrays

--- brocode-java/24-arrays/src/App.java
+++ brocode-java/24-arrays/src/App.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // array = a collection of values of the same data type
+        //         * think of it as a variable that can store more than 1 value *
+
+        String[] fruits = {"apple", "orange", "banana", "coconut"};
+
+        // fruits[0] = "pineapple";
+        // int numOfFruits = fruits.length;
+
+        Arrays.sort(fruits);  // sorts alphabetically
+        Arrays.fill(fruits, "pineapple");
+
+        for(int i = 0; i < fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }
+
+        System.out.println();
+
+        // enhanced for loop
+        for(String fruit : fruits) {
+            System.out.println(fruit);
+        }
     }
 }
