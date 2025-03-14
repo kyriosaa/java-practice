8bfe6cf75ef930b65cc67f63f4fc0eec326cbf64
kyriosaa <kyriosaaph@gmail.com>
2025-03-14 20:17:19 +0700

    26-varargs

--- brocode-java/26-varargs/src/App.java
+++ brocode-java/26-varargs/src/App.java
@@ -1,5 +1,36 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // varargs = Allow a method to accept a varying number of arguments.
+        //           Makes methods more flexible, no need for overloaded methods.
+        //           Java will pack the arguments into an array
+        //           ... (ellipsis)
+
+        System.out.println(add(1, 2, 3, 4));
+        System.out.println(average(1, 2, 3, 4));
+    }
+
+    static int add(int... numbers) {
+        int sum = 0;
+
+        for(int number : numbers) {
+            sum += number;
+        }
+
+        return sum;
+    }
+
+    static double average(double... numbers) {
+        double sum = 0;
+
+        if(numbers.length == 0) {
+            return 0;
+        } else {
+            for(double number : numbers) {
+                sum += number;
+            }
+    
+            return sum / numbers.length;
+        }        
     }
 }
