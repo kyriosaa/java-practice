49f310b186d084a4b0d0d29c111d5eaec8d2a227
kyriosaa <kyriosaaph@gmail.com>
2025-03-15 16:52:44 +0700

    31-object-arrays

--- brocode-java/31-object-arrays/src/App.java
+++ brocode-java/31-object-arrays/src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // Car car1 = new Car("Mustang", "Red");
+        // Car car2 = new Car("Corvette", "Blue");
+        // Car car3 = new Car("Charger", "Yellow"); 
+
+        Car[] cars = {new Car("Mustang", "Red"), 
+                      new Car("Corvette", "Blue"), 
+                      new Car("Charger", "Yellow")};
+
+        for(int i = 0; i < cars.length; i++) {
+            cars[i].drive();
+        }
+
+        System.out.println();
+
+        for(Car car : cars) {
+            car.drive();
+        }
+
     }
 }
