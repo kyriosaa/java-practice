3a91c96657f81f0c822ecdfc8417970aa9032461
kyriosaa <kyriosaaph@gmail.com>
2025-03-16 13:00:59 +0700

    34-super

--- brocode-java/34-super/src/App.java
+++ brocode-java/34-super/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // super = Refers to the parent class (subclass <- superclass).
+        //         Used in constructors and method overriding.
+        //         Calls the parent constructor to initialize attributes.
+
+        Person person = new Person("Harry", "Potter");
+        Student student = new Student("Ron", "Weasley", 3.5);
+        Employee employee = new Employee("Rubeus", "Hagrid", 50000);
+
+        person.showName();
+
+        student.showGPA();
+
+        employee.showSalary();
     }
 }
