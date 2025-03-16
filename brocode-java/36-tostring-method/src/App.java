a97c2152d00c64c77e243d318f1d66a211a0eefa
kyriosaa <kyriosaaph@gmail.com>
2025-03-16 14:44:07 +0700

    36-tostring-method

--- brocode-java/36-tostring-method/src/App.java
+++ brocode-java/36-tostring-method/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // .toString() = Method inherited from the object class.
+        //               Used to return a string representation of an object.
+        //               By default, it returns a hash code as a unique identifier.
+        //               It can be overridden to provide meaningful details.
+
+        Car car1 = new Car("Ford", "Mustang", 2025, "Red");
+        Car car2 = new Car("Chevrolet", "Corvette", 2026, "Blue");
+
+        System.out.println(car1);
+        System.out.println(car2);
+        
     }
 }
