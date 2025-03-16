36727f65cc80cd410b230134dd2d5d31335ebc2b
kyriosaa <kyriosaaph@gmail.com>
2025-03-16 12:38:45 +0700

    32-static

--- brocode-java/32-static/src/App.java
+++ brocode-java/32-static/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // static = Makes a variable or method belong to the class
+        //          rather than to any specific object.
+        //          Commonly used for utility methods or shared resources.
+
+        Friend friend1 = new Friend("Spongebob");
+        Friend friend2 = new Friend("Patrick");
+        Friend friend3 = new Friend("Squidward");
+        Friend friend4 = new Friend("Sandy");
+        Friend friend5 = new Friend("Gary");
+
+        System.out.println(Friend.numOfFriends);
+        Friend.showFriends();
+
+        Math.round(3.99);
     }
 }
