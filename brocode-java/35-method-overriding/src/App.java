21c1bcf49ddeaea761b97781777af6b66e6cc7c9
kyriosaa <kyriosaaph@gmail.com>
2025-03-16 13:06:30 +0700

    35-method-overriding

--- brocode-java/35-method-overriding/src/App.java
+++ brocode-java/35-method-overriding/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // method overriding = When a subclass provides its own implementation
+        //                     of a method that is already defined.
+        //                     Alllows for code reusability and give specific implementations.
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Fish fish = new Fish();
+
+        dog.move();
+        cat.move();
+        fish.move();
     }
 }
