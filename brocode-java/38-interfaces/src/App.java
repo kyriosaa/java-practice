b618984cc72d80102c40bad273ad7aaa5f37c151
kyriosaa <kyriosaaph@gmail.com>
2025-03-16 15:08:32 +0700

    38-interfaces

--- brocode-java/38-interfaces/src/App.java
+++ brocode-java/38-interfaces/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // interface = A blueprint for a class that specifies a set of
+        //             abstract methods that implementing classes MUST define.
+        //             Supports multiple inheritance-like behavior
+
+        Rabbit rabbit = new Rabbit();
+        Hawk hawk = new Hawk();
+        Fish fish = new Fish();
+
+        rabbit.flee();
+        hawk.hunt();
+
+        fish.flee();
+        fish.hunt();
     }
 }
