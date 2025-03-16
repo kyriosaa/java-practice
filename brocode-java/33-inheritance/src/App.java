6d1e8ef1f344e437b14afc9c33904884d4516804
kyriosaa <kyriosaaph@gmail.com>
2025-03-16 12:49:08 +0700

    33-inheritance

--- brocode-java/33-inheritance/src/App.java
+++ brocode-java/33-inheritance/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // inheritance = One class inherits the attributes
+        //               and methods from another class.
+        //               Child <- Parent <- Grandparent
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Plant plant = new Plant();
+
+        System.out.println(dog.isAlive);
+        System.out.println(cat.isAlive);
+        System.out.println(plant.isAlive);
+
+        plant.photosynthesize();
     }
 }
