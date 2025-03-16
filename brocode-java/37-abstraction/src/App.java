84ec0677a4a9dcbcffcdfa8ae51072818a08fad6
kyriosaa <kyriosaaph@gmail.com>
2025-03-16 15:01:06 +0700

    37-abstraction

--- brocode-java/37-abstraction/src/App.java
+++ brocode-java/37-abstraction/src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // abstraction = Used to define abstract classes and methods.
+        //               Abstraction is the process of hiding implementation details
+        //               and showing only the essential features.
+        //               Abstract classes CAN'T be instantiated directly.
+        //               Can contain 'abstract' methods (which must be implemented)
+        //               Can contain 'concrete' methods (which are inherited)
+
+        Circle circle = new Circle(3);
+        Triangle triangle = new Triangle(4, 5);
+        Rectangle rectangle = new Rectangle(6, 7);
+
+        circle.display();
+        triangle.display();
+        rectangle.display();
+
+        System.out.println();
+
+        System.out.println(circle.area());
+        System.out.println(triangle.area());
+        System.out.println(rectangle.area());
     }
 }
